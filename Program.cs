2f72e83f9fdfbff974e4e37daf1106c8641e129e
ArtemSafarian-Georgian <99339968+ArtemSafarian-Georgian@users.noreply.github.com>
2022-02-09 12:49:17 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,43 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Problem_set_1
+namespace Question_4
 {
+
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            object o  = 5;
-        Console.WriteLine(o.GetType());
-      
-        object V1  = 5.0;
-        Console.WriteLine(V1.GetType());
 
-         object V2  = 5>1;
-        Console.WriteLine(V2.GetType());
 
-        
-         object V3  = '5';
-        Console.WriteLine(V3.GetType());
+            int[] data = { 25, 47, 42, 56, 32 };
+            List<int> oddNumbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
 
-        
-         object V4  = 5*2;
-        Console.WriteLine(V4.GetType());
 
-        object V5  = 5/2;
-        Console.WriteLine(V5.GetType());
 
-        object V6  = 5%2;
-        Console.WriteLine(V6.GetType());
+            int i = 0;
+            foreach (var numbers in data)
+            {
 
-       
+                if (numbers % 2 != 0)
+                {
+                    oddNumbers.Add(numbers);
+                }
+                else
+                {
+                    evenNumbers.Add(numbers);
 
-         object V8  = 5==3;
-        Console.WriteLine(V8.GetType());
+                }
+                i++;
+            }
 
-        object V9  = (Math.PI);
-        Console.WriteLine(V9.GetType());
+            int[] oddArray = oddNumbers.ToArray();
+            int[] evenArray = evenNumbers.ToArray();
 
-        Console.WriteLine("{5,2,1} is an Invalid type ");
+
+            Console.WriteLine("The even elements are:");
+            foreach (var evenNumber in evenArray)
+            {
+                Console.WriteLine(evenNumber);
+            }
+
+            Console.WriteLine("The Odd elements are:");
+            foreach (var oddNumber in oddArray)
+            {
+                Console.WriteLine(oddNumber);
+            }
+
 
         }
     }
